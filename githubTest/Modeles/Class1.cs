dd338f2d16ce3cec5a95cec5bf764631e8d5ad50
tgallou <tgallou@bts-sio.local>
2019-11-05 08:53:12 +0100

    Ajoutez des fichiers projet.

---
+++ githubTest/Modeles/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubTest.Modeles
+{
+    class Class1
+    {
+        private int x = 0;
+    }
+}
